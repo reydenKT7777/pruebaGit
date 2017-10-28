5c9638200fb285951a4d72cb251e80ced463f5b9
reyden <the_evil_reyden@hotmail.com>
2017-10-28 12:38:46 -0400

    se añadio la segunda lista de accesos

---
+++ lista2.php
@@ -0,0 +1,15 @@
+<center><h3>Lista de accesos al sistema</h3></center>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Usuario</th>
+      <th>Hora</th>
+    </tr>
+  </thead>
+  <thead>
+    <tr>
+      <td>Jeferson Gutierritos</td>
+      <td>2017-10-2 14:15</td>
+    </tr>
+  </thead>
+</table>
