cd285d8498595f5dbbec48e3fc79e59821908884
reyden <the_evil_reyden@hotmail.com>
2017-10-28 12:42:44 -0400

    Se añadio la lista de archivos

---
+++ lista3.php
@@ -0,0 +1,15 @@
+<center><h3>Lista de archivos</h3></center>
+<table>
+  <thead>
+    <tr>
+      <th>Usuario</th>
+      <th>Archivo</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Jeferson Gutierritos</td>
+      <td>imagenes</td>
+    </tr>
+  </tbody>
+</table>
